94e64680db1f6f8891ae366a2eb57583804fa43f
Alex Schloegl <alexander.schloegl@compass.at>
2018-12-18 13:46:48 +0100

    Added price to transaction item

--- src/Transaction/Item/Creator.php
+++ src/Transaction/Item/Creator.php
@@ -2,16 +2,33 @@
 namespace Ecommerce\Transaction\Item;
 
 use Ecommerce\Common\EntityDtoCreator;
+use Ecommerce\Common\PriceCreator;
 use Ecommerce\Db\Transaction\Item\Entity;
 
 class Creator implements EntityDtoCreator
 {
+	/**
+	 * @var PriceCreator
+	 */
+	private $priceCreator;
+
+	/**
+	 * @param PriceCreator $priceCreator
+	 */
+	public function __construct(PriceCreator $priceCreator)
+	{
+		$this->priceCreator = $priceCreator;
+	}
+
 	/**
 	 * @param Entity $entity
 	 * @return Item
 	 */
 	public function byEntity($entity)
 	{
-		return new Item($entity);
+		return new Item(
+			$entity,
+			$this->priceCreator->fromCents($entity->getPrice(), $entity->getTax())
+		);
 	}
 }
--- src/Transaction/Item/Item.php
+++ src/Transaction/Item/Item.php
@@ -2,7 +2,9 @@
 namespace Ecommerce\Transaction\Item;
 
 use Common\Hydration\ArrayHydratable;
+use Ecommerce\Common\Price;
 use Ecommerce\Db\Transaction\Item\Entity;
+use Ramsey\Uuid\UuidInterface;
 
 class Item implements ArrayHydratable
 {
@@ -12,11 +14,38 @@ class Item implements ArrayHydratable
 	private $entity;
 
 	/**
-	 * @param Entity $entity
+	 * @ObjectToArrayHydratorProperty
+	 *
+	 * @var Price
 	 */
-	public function __construct(Entity $entity)
+	private $price;
+
+	/**
+	 * @param Entity $entity
+	 * @param Price $price
+	 */
+	public function __construct(Entity $entity, Price $price)
 	{
 		$this->entity = $entity;
+		$this->price  = $price;
+	}
+
+	/**
+	 * @return Price
+	 */
+	public function getPrice(): Price
+	{
+		return $this->price;
+	}
+
+	/**
+	 * @ObjectToArrayHydratorProperty
+	 *
+	 * @return UuidInterface
+	 */
+	public function getId()
+	{
+		return $this->entity->getId();
 	}
 
 	/**
