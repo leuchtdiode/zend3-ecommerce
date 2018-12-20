366f89f04395b2eb201edce3e587803bc25e43b5
Alex <alex@try2catch.com>
2018-12-20 20:58:36 +0100

    Return status code 400 if response is unsuccessful

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,10 @@
 <?php
 namespace Ecommerce;
 
+use Common\Util\StringUtil;
+use Zend\Mvc\MvcEvent;
+use Zend\View\Model\JsonModel;
+
 class Module
 {
 	/**
@@ -9,5 +13,42 @@ class Module
 	public function getConfig()
 	{
 		return include __DIR__ . '/../config/module.config.php';
+	}
+
+	/**
+	 * @param MvcEvent $e
+	 */
+	public function onBootstrap(MvcEvent $e)
+	{
+		$eventManager = $e
+			->getApplication()
+			->getEventManager();
+
+		$eventManager->attach(MvcEvent::EVENT_DISPATCH,
+			function () use ($e)
+			{
+				if (!StringUtil::startsWith(
+					$e
+						->getRequest()
+						->getRequestUri(),
+					'/ecommerce')
+				)
+				{
+					return;
+				}
+
+				$viewModel = $e->getViewModel();
+
+				if ($viewModel instanceof JsonModel && $viewModel->success === false)
+				{
+					$e
+						->getResponse()
+						->setStatusCode(400);
+
+					$e->stopPropagation(true);
+				}
+
+				return null;
+			});
 	}
 }
