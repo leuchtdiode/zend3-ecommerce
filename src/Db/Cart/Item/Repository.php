52047f44f6cb14d854b79511565147c52df37d1a
Alex <alex@try2catch.com>
2018-12-21 19:25:22 +0100

    Added endpoint and logic to remove cart item

--- src/Db/Cart/Item/Repository.php
+++ src/Db/Cart/Item/Repository.php
@@ -3,6 +3,9 @@ namespace Ecommerce\Db\Cart\Item;
 
 use Common\Db\EntityRepository;
 
+/**
+ * @method Entity find($id, $lockMode = null, $lockVersion = null)
+ */
 class Repository extends EntityRepository
 {
 
